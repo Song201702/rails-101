db519451d8c51be9d12e5ba9e9eadf6ae3278dc8
Chen Song <songminmin999@gmail.com>
2017-03-13 21:09:21 +0800

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,8 +5,12 @@ class GroupsController < ApplicationController
 
   def show
       @group = Group.find(params[:id])
-    end
-  
+  end
+
+  def edit
+   @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
   end
@@ -16,12 +20,12 @@ class GroupsController < ApplicationController
       @group.save
 
         redirect_to groups_path
-    end
+  end
 
-    private
+      private
 
-    def group_params
+  def group_params
       params.require(:group).permit(:title, :description)
-    end
+  end
 
 end
